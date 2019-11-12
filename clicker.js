b91b367d286660dad2afbffdb12d0fd59b9f6f48
Elodie Deshayes <elodied69@gmail.com>
2019-11-12 11:08:58 +0100

    first commit

--- clicker.js
+++ clicker.js
@@ -1,14 +1,20 @@
 const images = [
   "image1.gif",
   "image2.gif",
+  "image2.png",
   "image3.gif",
+  "image3bis.gif",
   "image4.gif",
   "image5.gif",
   "image6.gif",
   "image7.gif",
   "image8.gif",
+  "image8bis.gif",
   "image9.gif",
   "image10.gif",
+  "image12.png",
+  "image13.gif",
+  "image14.gif"
 ]
 
 let i = 0
